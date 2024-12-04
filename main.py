ef74baaf796419d42f8ffe650148c6f2296e9515
Emmanuel Guefif <eguefif@MacBook-Pro-de-De.local>
2024-12-03 22:02:08 -0500

    Extracted worker and make basic state machine

--- main.py
+++ main.py
@@ -1,48 +1,11 @@
 import asyncio
-
-
-def get_response(header):
-    length = len(header) + 2
-    h = b"HTTP/1.1 200 OK\r\n"
-    h += b"Date: Tue, 3 Dec 2024 21:16:3 EDT\r\n"
-    h += b"Accept_Ranges: bytes\r\n"
-    h += f"Content-length: #{length}\r\n".encode()
-    h += b"Vary: Accept-Encoding\r\n"
-    h += b"Content-Type: text/plain\r\n\r\n"
-    h += f"{header}\r\n\r\n".encode()
-    return h
-
-
-def get_request(req):
-    chunks = req.split(b" ")
-    retval = {
-        "method": chunks[0].decode().strip(),
-        "path": chunks[1].decode().strip(),
-        "protocol": chunks[2].decode().strip(),
-    }
-    return retval
-
-
-def parse_header(data):
-    splits = data.strip().split(b"\r\n")
-
-    header = {}
-    header["request"] = get_request(splits[0])
-    for chunk in splits[1:]:
-        parts = chunk.split(b":")
-        header[parts[0].decode().strip()] = parts[1].decode().strip()
-
-    return header
+from worker.worker import Worker
 
 
 async def handle(reader, writer):
-    data = await reader.read(500)
-    header = parse_header(data)
+    worker = Worker(reader, writer)
+    await worker.run()
 
-    response = get_response(header)
-    writer.write(response)
-
-    await writer.drain()
     writer.close()
     await writer.wait_closed()
 
---
+++ worker/worker.py
@@ -0,0 +1,84 @@
+MAX_READ = 10000
+
+
+class Worker:
+    def __init__(self, reader, writer):
+        self.reader = reader
+        self.writer = writer
+        self.state = "HEADER"
+
+    async def run(self):
+        buffer = b""
+        while self.state != "ENDING" and len(buffer) < MAX_READ:
+            buffer += await self.reader.read(1)
+            buffer = await self.handle_state(buffer)
+
+    async def handle_state(self, buffer):
+        if self.state == "HEADER":
+            if len(buffer) > 3 and buffer[-4:] == b"\r\n\r\n":
+                self.header = self.parse_header(buffer)
+                self.state = "BODY"
+                buffer = b""
+
+        if self.state == "BODY":
+            if not self.is_body():
+                self.state = "RESPONDING"
+            elif len(buffer) > 3 and buffer[-4:] == b"\r\n\r\n":
+                self.body = buffer.decode().strip
+                self.state = "RESPONDING"
+                buffer = b""
+
+        if self.state == "RESPONDING":
+            response = self.make_response()
+            self.writer.write(response.encode())
+            await self.writer.drain()
+            self.state = "ENDING"
+
+        return buffer
+
+    def is_body(self):
+        if self.header["request"]["method"] in ["POST", "UPDATE"]:
+            return True
+        else:
+            return False
+
+    def parse_header(self, data):
+        header = data.decode()
+        splits = header.strip().split("\r\n")
+
+        header = {}
+        header["request"] = self.get_request_line(splits[0])
+        for chunk in splits[1:]:
+            parts = chunk.split(":")
+            header[parts[0].strip()] = parts[1].strip()
+        return header
+
+    def get_request_line(self, first_line):
+        chunks = first_line.split(" ")
+        retval = {
+            "method": chunks[0].strip(),
+            "path": chunks[1].strip(),
+            "protocol": chunks[2].strip(),
+        }
+        return retval
+
+    def make_response(self):
+        response = ""
+        response += self.make_header()
+        response += self.make_body()
+
+        return response
+
+    def make_header(self):
+        length = len(self.header) + 2
+        header = "HTTP/1.1 200 OK\r\n"
+        header += "Date: Tue, 3 Dec 2024 21:16:3 EDT\r\n"
+        header += "Accept_Ranges: bytes\r\n"
+        header += f"Content-length: #{length}\r\n"
+        header += "Vary: Accept-Encoding\r\n"
+        header += "Content-Type: text/plain\r\n\r\n"
+
+        return header
+
+    def make_body(self):
+        return f"{self.header}\r\n\r\n"
